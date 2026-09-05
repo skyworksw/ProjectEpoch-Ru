b610b7e3f3630f4a245d9dcde66e5974ed31575a
Ivan Huzhva <skywork.work@email.com>
2026-09-05 14:48:33 +0200

    Update quest 26781 to include specific item name in objectives for clarity

--- Data/Custom/InterfaceData_Custom_RU.lua
+++ Data/Custom/InterfaceData_Custom_RU.lua
@@ -5,3 +5,13 @@ RUQL_GLOBAL_STRINGS = RUQL_GLOBAL_STRINGS or {}
 -- Примеры:
 -- RUQL_INTERFACE_TEXT["QuestFrameAcceptButton"] = "Принять"
 -- RUQL_GLOBAL_STRINGS["ACCEPT"] = "Принять"
+
+-- Стандартные строки тултипа предметов (действуют сразу на все предметы игры).
+RUQL_GLOBAL_STRINGS["ITEM_BIND_ON_PICKUP"] = "Привязывается при получении"
+RUQL_GLOBAL_STRINGS["ITEM_BIND_ON_EQUIP"] = "Привязывается при экипировке"
+RUQL_GLOBAL_STRINGS["ITEM_BIND_ON_USE"] = "Привязывается при использовании"
+RUQL_GLOBAL_STRINGS["ITEM_BIND_QUEST"] = "Квестовый предмет"
+RUQL_GLOBAL_STRINGS["ITEM_UNIQUE"] = "Уникальный"
+RUQL_GLOBAL_STRINGS["ITEM_SPELL_TRIGGER_ONUSE"] = "Использование: "
+RUQL_GLOBAL_STRINGS["ITEM_SPELL_TRIGGER_ONEQUIP"] = "Экипировка: "
+RUQL_GLOBAL_STRINGS["ITEM_SPELL_TRIGGER_CHANCE_ON_HIT"] = "Шанс при ударе: "
